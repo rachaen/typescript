eae515730458f8d07233f80bcf13cd0749b2e378
rachaen <rachaenlee@gmail.com>
2023-04-14 00:05:07 +0900

    기본 타입 연습

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,22 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        console.log('😃 loaded');
+        break;
+      case 'fail':
+        console.log('😱 no network');
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
