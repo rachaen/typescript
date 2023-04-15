ae22f0cf2a6bd5f074a87befc8e17f9759117792
DESKTOP-KVCQHCD\multicampus <codms1104@naver.com>
2023-04-16 01:09:52 +0900

    제네릭 연습

--- 6-generic-project/stack.ts
+++ 6-generic-project/stack.ts
@@ -1,32 +1,32 @@
 {
-  interface Stack {
+  interface Stack<T> {
     readonly size: number;
-    push(value: string): void;
-    pop(): string;
+    push(value: T): void;
+    pop(): T;
   }
 
-  type StackNode = {
-    readonly value: string;
-    readonly next?: StackNode;
+  type StackNode<T> = {
+    readonly value: T;
+    readonly next?: StackNode<T>;
   };
 
-  class StackImpl implements Stack {
+  class StackImpl<T> implements Stack<T> {
     private _size: number = 0;
-    private head?: StackNode;
+    private head?: StackNode<T>;
 
     constructor(private capacity: number) {}
     get size() {
       return this._size;
     }
-    push(value: string) {
+    push(value: T) {
       if (this.size === this.capacity) {
         throw new Error('Stack is full!');
       }
-      const node: StackNode = { value, next: this.head };
+      const node = { value, next: this.head };
       this.head = node;
       this._size++;
     }
-    pop(): string {
+    pop(): T {
       if (this.head == null) {
         throw new Error('Stack is empty!');
       }
@@ -37,7 +37,7 @@
     }
   }
 
-  const stack = new StackImpl(10);
+  const stack = new StackImpl<string>(10);
   stack.push('Ellie 1');
   stack.push('Bob 2');
   stack.push('Steve 3');
@@ -45,5 +45,11 @@
     console.log(stack.pop());
   }
 
-  stack.pop();
+  const stack2 = new StackImpl<number>(10);
+  stack2.push(123);
+  stack2.push(342);
+  stack2.push(13);
+  while (stack2.size !== 0) {
+    console.log(stack2.pop());
+  }
 }
