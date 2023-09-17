d01d5264f77da1de051a6ae06075bcc974f1640f
rachaen <rachaenlee@gmail.com>
2023-09-17 21:54:19 +0900

    디버깅 하는 방법

--- 11-config/build/dev.js
+++ 11-config/build/dev.js
@@ -1,1 +1,2 @@
 "use strict";
+//# sourceMappingURL=dev.js.map
---
+++ 11-config/build/logging/logging.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('tsc -w watch모드로 js 파일 생성');
+console.log('tsc init으로 tsconfig.json을 생성');
+//# sourceMappingURL=logging.js.map
